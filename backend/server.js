2d848340a925200a8da84513480a58154f0d5c9f
veerababuit <bveerababu.it@gmail.com>
2024-09-14 13:39:55 +0530

    frontend

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,19 @@
+// server.js
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db.js");
 dotenv.config();
 
 const app = express();
 
-//Middleware
+// Connect MongoDB
+connectDB();
 
+//Middleware
 app.use(cors());
 app.use(express.json());
-
-// MongoDB connection
-// username : bveerababuit
-// password : m2yuHRvI2xur5pKJ
-// password : 0asm1EPnaKoTRQBi
-// connection string :  mongodb+srv://bveerababuit:<db_password>@cluster0.9nwqq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 
 mongoose
   .connect(process.env.MONGO_URI, {
@@ -25,11 +23,11 @@ mongoose
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
-  //Routes
+//Routes
 
-  app.use('/api/auth', require('./routes/auth'));
-  app.use('/api/products', require('./routes/products'));
-  app.use('/api/dashboard', require('./routes/dashboard'));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/products", require("./routes/products"));
+app.use("/api/dashboard", require("./routes/dashboard"));
 
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, ()=> console.log(`Server running on PORT ${PORT}`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
